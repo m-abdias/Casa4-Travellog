a12b2add7dfb89b5f22496fd139945766b1ba024
Marcos9868 <marcos_m_ferreira@outlook.com>
2022-07-14 19:23:37 -0300

    Corrige endpoint /GET user_points e adiciona o endpoint /POST register

--- Backend/src/routes/user_points.route.js
+++ Backend/src/routes/user_points.route.js
@@ -8,14 +8,21 @@ userRoute.get('/query_api/v1/user_info', async (req, res, next) => {
   res.status(200).send(info)
 })
 
-userRoute.get('/query_api/v1/user_points/:user_id', async (req, res, next) => {
+userRoute.get('/query_api/v1/user_points/:id', async (req, res, next) => {
   try {
-    const id = req.params.user_id
+    const id = req.params.id
     const points = await queryInformation.getUserPoints(id)
     res.status(200).send(points)
   } catch (error) {
     next(error)
   }
+})
+
+userRoute.post('/query_api/v1/register', async(req, res, next) => {
+  const data = req.body
+  const newUser = await queryInformation.createUser(data)
+  res.sendStatus(201)
+  console.log(newUser)
 })
 
 module.exports = userRoute
