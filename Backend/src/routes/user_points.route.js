a565149442f4b58f5404f6a6a6972bdf991f3a80
Marcos9868 <marcos_m_ferreira@outlook.com>
2022-07-14 14:56:07 -0300

    Adiciona endpoint /GET que busca os pontos do usuário pelo id

--- Backend/src/routes/user_points.route.js
+++ Backend/src/routes/user_points.route.js
@@ -3,9 +3,19 @@ const queryInformation = require('../queries/user_query')
 
 const userRoute = router()
 
-userRoute.get('/query_api/v1/user_info', async(req, res, next) => {
+userRoute.get('/query_api/v1/user_info', async (req, res, next) => {
   const info = await queryInformation.getUserInfo()
   res.status(200).send(info)
+})
+
+userRoute.get('/query_api/v1/user_points/:user_id', async (req, res, next) => {
+  try {
+    const id = req.params.user_id
+    const points = await queryInformation.getUserPoints(id)
+    res.status(200).send(points)
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = userRoute
