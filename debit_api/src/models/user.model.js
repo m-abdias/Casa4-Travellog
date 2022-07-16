e6bd6554fcbb9d1269302f5abcbd539f7e0f7111
Marcos9868 <marcos_m_ferreira@outlook.com>
2022-07-16 18:07:11 -0300

    Adiciona o contrato que a API deve obedecer quando fizer as requisições

---
+++ debit_api/src/models/user.model.js
@@ -0,0 +1,9 @@
+const User = (id, name, email, password, points) => {
+  this.id = id
+  this.name = name
+  this.email = email
+  this.password = password
+  this.points = points
+}
+
+module.exports = User
