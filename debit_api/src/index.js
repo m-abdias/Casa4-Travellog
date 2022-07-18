f7e98d0608acd21b578c305c0a4d83d150927370
Marcos9868 <marcos_m_ferreira@outlook.com>
2022-07-18 11:42:25 -0300

    Insere e utiliza o ErrorHandler na API

--- debit_api/src/index.js
+++ debit_api/src/index.js
@@ -1,6 +1,8 @@
 const debitApi = require('./config/server')
 const testRoute = require('./routes/test.route')
 const debitRoute = require('./routes/debit.route')
+const ErrorHandler = require('./middlewares/error.handler.middleware')
 
 debitApi.use(testRoute)
 debitApi.use(debitRoute)
+debitApi.use(ErrorHandler)
