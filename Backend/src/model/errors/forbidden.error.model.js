5fe8f46f73b9d101966503ebaad9b9047eb84f1e
Marcos9868 <marcos_m_ferreira@outlook.com>
2022-07-19 17:27:13 -0300

    Adiciona modelo de erro para tentativa de acesso

---
+++ Backend/src/model/errors/forbidden.error.model.js
@@ -0,0 +1,10 @@
+class ForbiddenError extends Error {
+  constructor(message, error) {
+    super(message)
+
+    this.message = message,
+    this.error = error
+  }
+}
+
+module.exports = ForbiddenError
